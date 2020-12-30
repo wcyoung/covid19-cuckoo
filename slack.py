519460da430fe625c312659faaf7cdcd3be438cc
wcyoung <wcyoung98@gmail.com>
2020-12-31 02:16:47 +0900

    slack 메시지 형식 변경

--- slack.py
+++ slack.py
@@ -21,9 +21,9 @@ def post_message():
     attachments_dict['title_link'] = 'http://ncov.mohw.go.kr/'
     attachments_dict['fallback'] = f'[{yesterday}] 코로나 확진자 알림'
     attachments_dict['text'] = f"""*누적*: {INFO["total"]}명
-    *소계*: {INFO["sub_total"]}명
-    *국내발생*: {INFO["local"]}명
-    *해외유입*: {INFO["inflow"]}명"""
+*소계*: {INFO["sub_total"]}명
+*국내발생*: {INFO["local"]}명
+*해외유입*: {INFO["inflow"]}명"""
     attachments = [attachments_dict]
 
     slack.chat.post_message(channel=CONFIG['channel'], text=None, attachments=attachments, as_user=True)
